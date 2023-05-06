84faa670b2da1a514d83533e8c0af34e8526846b
Angelica <lisoveckaya1997@gmail.com>
2023-05-06 13:54:37 +0300

    Update Statistics.jsx

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,14 +2,14 @@
 import PropTypes from 'prop-types';
 import css from './statistics.module.css';
 
-export const Statistics = props => {
+export const Statistics = ({good, neutral, bad, total, positivePercentage }) => {
   return (
     <ul className={css.list}>
-      <li className={css.listItem}> Good: {props.good} </li>
-      <li className={css.listItem}> Neutral: {props.neutral} </li>
-      <li className={css.listItem}> Bad: {props.bad} </li>
-      <li className={css.listItem}> Total: {props.total}</li>
-      <li className={css.listItem}> Positive feedback: {props.positivePercentage} %</li>
+      <li className={css.listItem}> Good: {good} </li>
+      <li className={css.listItem}> Neutral: {neutral} </li>
+      <li className={css.listItem}> Bad: {bad} </li>
+      <li className={css.listItem}> Total: {total}</li>
+      <li className={css.listItem}> Positive feedback: {positivePercentage} %</li>
     </ul>
   );
 };
